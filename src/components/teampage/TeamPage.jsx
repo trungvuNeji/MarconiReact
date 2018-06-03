4dbb7c750ecab22a06d8942923bbaafc4a2586a2
Trung Vu <trungvuh@gmail.com>
2018-06-03 16:25:07 -0700

    Fixing Chinese margin on team page

--- src/components/teampage/TeamPage.jsx
+++ src/components/teampage/TeamPage.jsx
@@ -8,6 +8,11 @@ import CoreTeam from './CoreTeam';
 import Advisor from './Advisor';
 
 class TeamPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.checkLanguage = this.checkLanguage.bind(this);
+  }
 
   componentDidMount() {
     document.title = "Marconi - Team";
@@ -23,13 +28,29 @@ class TeamPage extends Component {
     navBar.classList.add('sticky');
   }
 
+  checkLanguage() {
+    var lng = i18n.language;
+    // Fixing Chinese margin
+    var navbarNav = document.querySelector('.navbar-nav');
+    var footerNav = document.querySelector('.footer-nav');
+
+    if (lng === 'cn') {
+      navbarNav.classList.add('navbar-nav-cn');
+      footerNav.classList.add('footer-nav-cn');
+    } 
+    else if (lng === 'en') {
+      navbarNav.classList.remove('navbar-nav-cn');
+      footerNav.classList.remove('footer-nav-cn');
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="outer-box blue-bg col-xs-12 no-padding">
           <section className="container">
             <div className="row">
-              <Header />
+              <Header checkLanguage={this.checkLanguage}/>
             </div>
           </section>
         </div>
