3bc2c5812428d1427f5599cbbfcd10e83adb4367
Trung Vu <trungvuh@gmail.com>
2018-06-11 09:40:58 -0700

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 // this is out entry file, render it to the React DOM
+import 'babel-polyfill';
+import 'core-js';
+import 'raf/polyfill';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/fn/reflect/apply';
+import 'airbnb-js-shims';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,11 +25,6 @@ import i18n from './utils/i18n';
 
 import configureStore from './utils/store';
 import Root from './components/root.jsx';
-
-import "babel-polyfill";
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'raf/polyfill';
 
 import registerServiceWorker from './registerServiceWorker';
 
