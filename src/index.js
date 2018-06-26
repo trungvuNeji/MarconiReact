89430f8f4df86e58e6b73dcc73c30437a1260416
Trung Vu <t@neji.io>
2018-06-26 10:06:38 -0700

    Remove dependencies

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import 'babel-polyfill';
 import 'core-js';
 import 'es5-shim';
 import 'es6-shim';
-// import 'core-js/es5';
-// import 'core-js/es6/object';
-// import 'core-js/es6/reflect';
-// import 'web-animations-js';
+import 'raf/polyfill';
+import 'web-animations-js';
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -30,21 +29,3 @@ document.addEventListener("DOMContentLoaded", () => {
     </I18nextProvider>, root);
   registerServiceWorker();
 });
-
-
-// import 'raf/polyfill';
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
-// import 'core-js/es6/weak-set';
